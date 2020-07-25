bf180d8f551a163f51fa80c97740818e3275de50
JupiterSmokes <daniel-albizurezalpirez@cunoc.edu.gt>
2020-07-25 13:26:12 -0600

    Se vacio el documento

--- src/main/java/modelo/analizador.java
+++ src/main/java/modelo/analizador.java
@@ -10,36 +10,6 @@ package modelo;
  * @author DANIEL
  */
 public class analizador {
-    String cadena1;
-    String cadena2;
-
-    public analizador(String cadena1, String cadena2) {
-        this.cadena1 = cadena1;
-        this.cadena2 = cadena2;
-    }
-
-    public analizador() {
-    }
-
-    public String analizar(){
-        String patron = "";
-        
-        return patron;
-    }
-    
-    public String getCadena1() {
-        return cadena1;
-    }
-    public void setCadena1(String cadena1) {
-        this.cadena1 = cadena1;
-    }
-
-    public String getCadena2() {
-        return cadena2;
-    }
-    public void setCadena2(String cadena2) {
-        this.cadena2 = cadena2;
-    }
     
     
 }
