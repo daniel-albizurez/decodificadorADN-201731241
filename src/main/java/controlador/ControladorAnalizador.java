1499850725e01044febb3e7ae430cd86d4c42eee
JupiterSmokes <daniel-albizurezalpirez@cunoc.edu.gt>
2020-07-26 13:35:07 -0600

    #

--- src/main/java/controlador/ControladorAnalizador.java
+++ src/main/java/controlador/ControladorAnalizador.java
@@ -5,7 +5,10 @@
  */
 package controlador;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Scanner;
+import javax.swing.JOptionPane;
 import modelo.Analizador;
 import vista.JFrmAnalizador;
 
@@ -13,7 +16,7 @@ import vista.JFrmAnalizador;
  *
  * @author DANIEL
  */
-public class ControladorAnalizador {
+public class ControladorAnalizador implements ActionListener{
     
     private Analizador analizador = new Analizador();
     private JFrmAnalizador vista = new JFrmAnalizador();
@@ -21,6 +24,7 @@ public class ControladorAnalizador {
     public ControladorAnalizador(JFrmAnalizador vista){
         this.vista = vista;
         this.vista.setVisible(true);
+        this.vista.jBtnAnalizar.addActionListener(this);
     }
     
     public ControladorAnalizador() {
@@ -30,7 +34,15 @@ public class ControladorAnalizador {
         System.out.println("El conjunto de bases adyacentes de mayor tamaño es: "
                 + analizador.analizar(scanner.nextLine(), scanner.nextLine()));
     }
-    
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == vista.jBtnAnalizar) {
+            JOptionPane.showMessageDialog(vista, analizador.analizar(
+                    vista.jTxtCadena1.getText(),
+                    vista.jTxtCadena2.getText()));
+        }
+    }
     
 
         
