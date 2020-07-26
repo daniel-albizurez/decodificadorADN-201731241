b4a79f5a120137a35b7df8533efd14549e0e0a0d
JupiterSmokes <daniel-albizurezalpirez@cunoc.edu.gt>
2020-07-26 12:36:44 -0600

    Se agregó el controlador, proyecto ya funcional

--- src/main/java/controlador/ControladorAnalizador.java
+++ src/main/java/controlador/ControladorAnalizador.java
@@ -12,17 +12,20 @@ import modelo.Analizador;
  *
  * @author DANIEL
  */
-public class Ejecutar {
+public class ControladorAnalizador {
+    
+        private Analizador analizador = new Analizador();
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
+    public ControladorAnalizador() {
         Scanner scanner = new Scanner(System.in);
-        Analizador analizador = new Analizador();
-        System.out.println("Ingrese las cadenas a analizar (Presionando enter después de cada cadena)");
+        System.out.println("Ingrese las cadenas a analizar (Presione la tecla ENTER "
+                + "después de cada cadena)");
         System.out.println("El conjunto de bases adyacentes de mayor tamaño es: "
                 + analizador.analizar(scanner.nextLine(), scanner.nextLine()));
     }
     
+    
+
+        
+        
 }
---
+++ src/main/java/tarea1_labIPC2/TareaLabIPC2.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea1_labIPC2;
+
+import controlador.ControladorAnalizador;
+import java.util.Scanner;
+import modelo.Analizador;
+
+/**
+ *
+ * @author DANIEL
+ */
+public class TareaLabIPC2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ControladorAnalizador controlador = new ControladorAnalizador();
+    }
+    
+}
