5df6446b8730b95234db754fadbdbd9ae4c427ac
JupiterSmokes <daniel-albizurezalpirez@cunoc.edu.gt>
2020-07-26 19:39:27 -0600

    Correcciones

--- src/main/java/controlador/ControladorAnalizador.java
+++ src/main/java/controlador/ControladorAnalizador.java
@@ -9,23 +9,23 @@ import java.util.Scanner;
 import modelo.Analizador;
 
 /**
- *
+ * Clase Controlador, intermediaria entre la clase main y el objeto Analizador
  * @author DANIEL
  */
-public class ControladorAnalizador {
+public class ControladorAnalizador{
     
-        private Analizador analizador = new Analizador();
+    private Analizador analizador = new Analizador();
 
+    /**
+     * Crea un nuevo objeto Controlador
+     */
     public ControladorAnalizador() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Ingrese las cadenas a analizar (Presione la tecla ENTER "
                 + "después de cada cadena)");
-        System.out.println("El conjunto de bases adyacentes de mayor tamaño es: "
-                + analizador.analizar(scanner.nextLine(), scanner.nextLine()));
+        System.out.println(analizador.analizar(scanner.nextLine(), scanner.nextLine()));
     }
     
-    
-
-        
+ 
         
 }
